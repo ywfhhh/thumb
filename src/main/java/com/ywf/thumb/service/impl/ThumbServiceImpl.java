76dbc936b3c568c9efc51102410e1094a25aa294
yiwenfeng <1918346819@qq.com>
2025-04-19 22:47:44 +0800

    用户点赞接口-redisson分布式锁实现: 1.key为userId+BlogId 2.使用编程式事务 3.避免当前线程死锁，设置lease time过期时间，不使用看门狗机制

--- src/main/java/com/ywf/thumb/service/impl/ThumbServiceImpl.java
+++ src/main/java/com/ywf/thumb/service/impl/ThumbServiceImpl.java
@@ -13,15 +13,20 @@ import com.ywf.thumb.model.dto.DoThumbRequest;
 import com.ywf.thumb.service.BlogService;
 import com.ywf.thumb.service.ThumbService;
 import com.ywf.thumb.service.UserService;
+import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.support.TransactionTemplate;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
 @Slf4j
-@RequiredArgsConstructor
+@RequiredArgsConstructor // 自动生成构造函数进行依赖注入
 public class ThumbServiceImpl extends ServiceImpl<ThumbMapper, Thumb> implements ThumbService {
 
     private final UserService userService;
@@ -29,6 +34,8 @@ public class ThumbServiceImpl extends ServiceImpl<ThumbMapper, Thumb> implements
     private final BlogService blogService;
 
     private final TransactionTemplate transactionTemplate;
+
+    private final RedissonClient redissonClient;
 
     @Override
     public Boolean doThumb(DoThumbRequest doThumbRequest, HttpServletRequest request) {
@@ -39,9 +46,15 @@ public class ThumbServiceImpl extends ServiceImpl<ThumbMapper, Thumb> implements
         // 加锁
         //必须让锁的作用域完全包裹事务操作，保证其他线程获取锁时，前序事务必然已完成提交。
         // 即：先获取锁 → 开启事务 → 执行业务逻辑 → 提交事务 → 释放锁，保证点赞操作的原子性和一致性及事务的完整性不被其他线程干扰。
-        synchronized (loginUser.getId().toString().intern()) {
-
-            // 编程式事务
+        String key = String.format("thumb:blog:%d:%d", loginUser.getId(), doThumbRequest.getBlogId());
+        RLock lock = redissonClient.getLock(key);
+        boolean locked = false;
+        try {
+            // 不使用无参函数 看门狗续期机制， 等待最多 5 秒去拿锁，拿到锁后 30 秒自动解锁（防止死锁）
+            locked = lock.tryLock(5, 30, TimeUnit.SECONDS);
+            if (!locked) {
+                throw new ThumbException(ErrorCode.OPERATION_ERROR, "操作频繁!");
+            }
             return transactionTemplate.execute(status -> {
                 Long blogId = doThumbRequest.getBlogId();
                 boolean exists = this.lambdaQuery()
@@ -56,35 +69,85 @@ public class ThumbServiceImpl extends ServiceImpl<ThumbMapper, Thumb> implements
                         .eq(Blog::getId, blogId)
                         .setSql("thumbCount = thumbCount + 1")
                         .update();
-
+                if (!update) {
+                    throw new ThumbException(ErrorCode.OPERATION_ERROR, "更新点赞数失败!");
+                }
                 Thumb thumb = new Thumb();
                 thumb.setUserId(loginUser.getId());
                 thumb.setBlogId(blogId);
-                // 更新成功才执行
-                return update && this.save(thumb);
+                return this.save(thumb);
             });
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (locked && lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
         }
     }
+//        synchronized (loginUser.getId().toString().intern()) {
+//
+//            // 编程式事务
+//            return transactionTemplate.execute(status -> {
+//                Long blogId = doThumbRequest.getBlogId();
+//                boolean exists = this.lambdaQuery()
+//                        .eq(Thumb::getUserId, loginUser.getId())
+//                        .eq(Thumb::getBlogId, blogId)
+//                        .exists();
+//                if (exists) {
+//                    throw new ThumbException(ErrorCode.OPERATION_ERROR, "用户已点赞");
+//                }
+//
+//                boolean update = blogService.lambdaUpdate()
+//                        .eq(Blog::getId, blogId)
+//                        .setSql("thumbCount = thumbCount + 1")
+//                        .update();
+//
+//                Thumb thumb = new Thumb();
+//                thumb.setUserId(loginUser.getId());
+//                thumb.setBlogId(blogId);
+//                // 更新成功才执行
+//                return update && this.save(thumb);
+//            });
+//        }
+//    }
 
     @Override
     public Boolean undoThumb(DoThumbRequest doThumbRequest, HttpServletRequest request) {
         User loginUser = userService.getLoginUser(request);
-        // 编程式事务
-        return transactionTemplate.execute(status -> {
-            Long blogId = doThumbRequest.getBlogId();
-
-            Thumb thumb = this.lambdaQuery().eq(Thumb::getUserId, loginUser.getId()).eq(Thumb::getBlogId, blogId).one();
-            if (ObjectUtil.isNull(thumb)) {
-                throw new ThumbException(ErrorCode.OPERATION_ERROR, "用户未点赞");
+        String key = String.format("thumb:blog:%d:%d", loginUser.getId(), doThumbRequest.getBlogId());
+        RLock lock = redissonClient.getLock(key);
+        boolean locked = false;
+        try {
+            locked = lock.tryLock(5, 30, TimeUnit.SECONDS);
+            if (!locked) {
+                throw new ThumbException(ErrorCode.OPERATION_ERROR, "获取点赞分布式锁失败!");
             }
+            return transactionTemplate.execute(status -> {
+                Long blogId = doThumbRequest.getBlogId();
+                Thumb thumb = this.lambdaQuery()
+                        .eq(Thumb::getUserId, loginUser.getId())
+                        .eq(Thumb::getBlogId, blogId)
+                        .one();
+                if (ObjectUtil.isNull(thumb)) {
+                    throw new ThumbException(ErrorCode.OPERATION_ERROR, "用户未点赞");
+                }
 
-            boolean update = blogService.lambdaUpdate()
-                    .eq(Blog::getId, blogId)
-                    .setSql("thumbCount = thumbCount - 1")
-                    .update();
-
-            // 更新成功才执行
-            return update && this.removeById(thumb.getId());
-        });
+                boolean update = blogService.lambdaUpdate()
+                        .eq(Blog::getId, blogId)
+                        .setSql("thumbCount = thumbCount - 1")
+                        .update();
+                if (!update) {
+                    throw new ThumbException(ErrorCode.OPERATION_ERROR, "更新点赞数失败!");
+                }
+                return this.removeById(thumb);
+            });
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (locked && lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
+        }
     }
 }
