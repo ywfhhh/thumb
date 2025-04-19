fc84c732681c33a5730d6851c4ffc55b7896d051
yiwenfeng <1918346819@qq.com>
2025-04-19 16:29:53 +0800

    点赞系统第一次提交

---
+++ src/main/java/com/ywf/thumb/ThumbApplication.java
@@ -0,0 +1,13 @@
+package com.ywf.thumb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThumbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThumbApplication.class, args);
+    }
+
+}
